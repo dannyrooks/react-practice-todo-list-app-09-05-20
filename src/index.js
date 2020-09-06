71bfb36dcbf9518498dc9ff8b62e06534eed040d
Daniel Rooker <dannyrooks@gmail.com>
2020-09-05 22:43:18 -0400

    2

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+// import TodoList from './TodoList';
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <App />, document.getElementById('root')
+);
+
+// youtube vid for this build https://youtu.be/hQAHSlTtcmY
